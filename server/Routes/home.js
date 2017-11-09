af6f9bf3fb639831fbf6ccf84b9047c86eb0da45
Pramudit <spramudit@gmail.com>
2017-11-10 01:31:51 +0530

    Login

--- server/Routes/home.js
+++ server/Routes/home.js
@@ -13,10 +13,26 @@ router.post('/signup', function(req,res,next){
   var user = new User(body)
 
   user.save().then((user) => {
-    res.send(user)
+    return user.generateAuthToken()
+  }).then((token) =>{
+      res.header('x-auth',token).send(user)
+    }).catch((e) => {
+      res.status(400).send(e)
+    })
     console.log(req.body);
-  })
+
 })
+
+router.post('/login', function(req,res,next){
+  var body = _.pick(req.body, ['username','password'])
+  User.findByCredentials(body.username, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 
 module.exports = router
--- server/models/users.js
+++ server/models/users.js
@@ -32,18 +32,32 @@ var UserSchema = new mongoose.Schema({
     //required: true,
     minlength: 6
   },
-  // tokens: [{
-  //   access: {
-  //     type: String,
-  //     //required: true
-  //   },
-  //   token: {
-  //     type:String,
-  //     //required: true
-  //   }
-  // }]
+  tokens: [{
+    access: {
+      type: String,
+      //required: true
+    },
+    token: {
+      type:String,
+      //required: true
+    }
+  }]
 
 })
+
+UserSchema.methods.generateAuthToken = function() {
+  var user= this;
+  var access = 'auth';
+
+  var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
+  user.tokens.push({
+    access: access,
+    token: token
+  })
+  return user.save().then(() => {
+    return token
+  })
+}
 UserSchema.pre('save', function(next){
   var user = this;
 
@@ -60,6 +74,21 @@ UserSchema.pre('save', function(next){
   }
 
 })
+
+UserSchema.statics.findByCredentials = function (username,password){
+  var User = this;
+  return User.findOne({username:username}).then((user) => {
+    if(!user){ return Promise.reject()}
+
+    return new Promise((resolve,reject) => {
+      bcrypt.compare(password,user.password,(err, res) => {
+        if(res){
+          resolve(user)
+        }else {reject()}
+      })
+    })
+  })
+}
 var User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
