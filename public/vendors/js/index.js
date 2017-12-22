3d648ca07eafd6e583477e9ac0ee0878d047cd95
Pramudit <spramudit@gmail.com>
2017-12-23 05:05:26 +0530

    ckahbj

--- public/vendors/js/index.js
+++ public/vendors/js/index.js
@@ -1,6 +1,6 @@
 $('.contact-form').on('submit',function(e) {
       e.preventDefault();
-      $.post( 'https://powerful-atoll-14808.herokuapp.com/signup', $('.contact-form').serialize(), function( data ) {
+      $.post( '/signup', $('.contact-form').serialize(), function( data ) {
            alert( "your request has been submitted" );
       } );
 });
