5236d0bec72bd6fb78a530dce48dffe3e11b2bd5
Pramudit <spramudit@gmail.com>
2017-12-23 05:32:17 +0530

    bkjbk

--- public/vendors/js/index.js
+++ public/vendors/js/index.js
@@ -1,6 +1,9 @@
 $('.contact-form').on('submit',function(e) {
       e.preventDefault();
-      $.post( '/signup', $('.contact-form').serialize(), function( data ) {
+      $.post( '/signup', $('.contact-form').serialize(),$(".contact-form")[0].reset(), function( data ) {
            alert( "your request has been submitted" );
       } );
+
+
+
 });
